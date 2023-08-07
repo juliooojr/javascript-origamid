8c9676e5ac14a3c82612a101b333a4f84ef25e35
juliooojr <juliojr0410@gmail.com>
2023-08-07 16:40:37 -0300

    first commit

---
+++ js-para-iniciantes/01-variaveis/aula01.js
@@ -0,0 +1,24 @@
+// Declarar uma variável com o seu nome
+var nome = 'julio'
+
+// Declarar uma variável com a sua idade
+var idade = 28
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+
+var comidaFavorita
+
+// Atribuir valor a sua comida favorita
+
+comidaFavorita = 'churrasco'
+
+// Declarar 5 variáveis diferentes sem valores
+
+var time,
+    jogador,
+    numero,
+    profissao,
+    mae
+
+console.log(nome, idade, comidaFavorita, time, jogador, numero, profissao, mae, comidaFavorita)
